a99689fd19f8504f986e8a6cd2d5d6cb0980c418
naka16bit <naka@local.me>
2022-08-10 00:20:26 +0300

    first commit

---
+++ element-search/menu/task.js
@@ -0,0 +1,29 @@
+let mainMenus = document.getElementsByClassName("menu_main");
+for (let mainMenu of mainMenus) {
+    let menuItems = mainMenu.querySelectorAll("ul.menu_main li.menu__item");
+    for (let menuItem of menuItems) {
+        let subMenus = menuItem.querySelectorAll("ul.menu_sub");
+        for (let subMenu of subMenus) {
+            menuItem.onclick = () => {
+                
+                if (subMenu.classList.contains("menu_active")) {
+                    deactivate();
+                    return false;
+                }
+                else {
+                    deactivate();
+                    subMenu.classList.add("menu_active");
+                    return false;
+                }
+            }
+        }
+    }
+}
+
+function deactivate() {
+    let activeMenu = document.querySelector("ul.menu_active");
+     if (activeMenu !== null) {
+        activeMenu.classList.remove("menu_active");
+     }
+    
+}
