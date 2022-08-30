5b56025e48111f434ca283c6bd907ab742240bec
naka16bit <naka@local.me>
2022-08-30 22:37:14 +0300

    first commit

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,53 @@
+const elements = document.querySelectorAll(".has-tooltip");
+const positions = {
+    top: function(tooltip, element) {
+        const rectTooltip = tooltip.getBoundingClientRect();
+        const rectElement = element.getBoundingClientRect();
+        tooltip.style.left = `${rectElement.left}px`;
+        tooltip.style.top = `${rectElement.top - rectTooltip.height}px`;
+    },
+    left: function(tooltip, element) {
+        const rectTooltip = tooltip.getBoundingClientRect();
+        const rectElement = element.getBoundingClientRect();
+        tooltip.style.left = `${rectElement.left - rectTooltip.width}px`;
+        tooltip.style.top = `${rectElement.top}px`;
+    },
+    right: function(tooltip, element) {
+        const rect = element.getBoundingClientRect();
+        tooltip.style.left = `${rect.right}px`;
+        tooltip.style.top = `${rect.top}px`;
+    },
+    bottom:  function(tooltip, element) {
+        const rectElement = element.getBoundingClientRect();
+        tooltip.style.left = `${rectElement.left}px`;
+        tooltip.style.top = `${rectElement.bottom}px`;
+    }
+}
+
+elements.forEach(element => { 
+    element.addEventListener("click", (e) => {
+        const oldTooltip = document.querySelector(".tooltip");
+        if (oldTooltip !== null) {
+            oldTooltip.remove();
+        }
+        showTooltip(element, "top");
+        e.preventDefault();
+    })
+})
+
+function showTooltip(element, position) {
+    const tooltip = document.createElement("div");
+    tooltip.innerText = element.getAttribute("title");
+    tooltip.classList.add("tooltip", "tooltip_active");
+    tooltip.setAttribute("position", position);
+    element.insertAdjacentElement("afterEnd", tooltip);
+
+    positions[position](tooltip, element);
+}
+
+window.addEventListener("scroll", () => {
+    const tooltipActive = document.querySelector(".tooltip_active");
+    if (tooltipActive !== null) {
+        tooltipActive.classList.remove("tooltip_active");
+    };
+})
