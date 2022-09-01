98ebf6cea0f07b026349633abac4035f5e4ea613
naka16bit <naka@local.me>
2022-09-02 00:24:22 +0300

    some things improved

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,17 +1,28 @@
-const tasksList = document.getElementById('tasks__list');
-const tasksAdd = document.getElementById('tasks__add');
-const taskInput = document.getElementById('task__input');
+const tasksList = document.getElementById("tasks__list");
+const tasksAdd = document.getElementById("tasks__add");
+const taskInput = document.getElementById("task__input");
 
-tasksAdd.addEventListener("click", (e) => {    
-    if (taskInput.value === "") return;
-    
-    tasksList.innerHTML += 
+tasksAdd.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (taskInput.value.trim() === "") {
+        taskInput.value = "";
+        return;
+    }
+
+    tasksList.insertAdjacentHTML("beforeEnd", 
     `<div class="task">
         <div class="task__title">
             ${taskInput.value}
         </div>
-        <a href="#" class="task__remove" onclick="this.closest('.task').outerHTML = ''">&times;</a>
-    </div>`;
+        <a href="#" class="task__remove">&times;</a>
+    </div>`);
     taskInput.value = "";
-    e.preventDefault();
+
+    const taskRemoves = document.querySelectorAll(".task__remove");
+    taskRemoves.forEach(taskRemove => {
+        taskRemove.addEventListener("click", (e) => {
+            e.preventDefault();
+            taskRemove.closest(".task").remove();
+        });
+    });
 });
